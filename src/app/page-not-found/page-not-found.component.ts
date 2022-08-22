e357c25aa1e6ba479553d0be57f15e8d3a348f10
LaureCHAPERT <laure.chapert@gmail.com>
2022-08-22 11:58:39 +0200

    Added first route and first component

---
+++ src/app/page-not-found/page-not-found.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'page-404',
+    template: `
+    <div class='center'>
+      <img src="https://cdn.pixabay.com/photo/2016/04/24/22/30/monitor-1350918_960_720.png"/>
+      <h1>Cette page n'existe pas !</h1>
+      <a routerLink="/pokemons" class="waves-effect waves-teal btn-flat">
+        Retourner à l' accueil
+      </a>
+    </div>
+  `
+})
+export class PageNotFoundComponent { }
