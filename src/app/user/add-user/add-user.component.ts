a994cc800f722deb82c0cc06d0b05913e414a558
LaureCHAPERT <laure.chapert@gmail.com>
2022-08-24 10:01:35 +0200

    delete method  is functional but adding still in progress

--- src/app/user/add-user/add-user.component.ts
+++ src/app/user/add-user/add-user.component.ts
@@ -4,12 +4,14 @@ import { User } from 'src/app/user';
 @Component({
   selector: 'app-add-user',
   template: `
-    <h2 class="center">Ajouter un utilisateur</h2>
+    <h2 class="center">Ajouer un utilisateur</h2>
     <app-user-form [user]="user"></app-user-form>
   `,
 })
 export class AddUserComponent implements OnInit {
+
   user:User;
+
   ngOnInit(): void {
     this.user = new User();
   }
